b256d487a832bc16a6453d010b21f059007a4eb8
Floias <irina.butcher@gmail.com>
2024-08-08 22:05:15 +0500

    оживляет первый слайдер

--- source/js/modules/hero-slider.js
+++ source/js/modules/hero-slider.js
@@ -4,14 +4,16 @@ import { Pagination } from 'swiper/modules';
 
 const initHeroSlider = () => {
   const swiper = new Swiper('.hero__container', {
-    direction: 'vertical',
+    modules: [Pagination],
+    direction: 'horizontal',
+    slidesPerView: 1,
     init: false,
     slideClass: 'hero__item',
     wrapperClass: 'hero__list',
     loop: true,
-    modules: [Pagination],
     pagination: {
       el: '.hero__pagination',
+      clickable: true,
     },
   });
 
