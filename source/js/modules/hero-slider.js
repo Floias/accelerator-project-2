9e118b36b7c26c19ce9c99af45168da400e4056d
Floias <irina.butcher@gmail.com>
2024-08-09 16:01:23 +0500

    заканчивает hero

--- source/js/modules/hero-slider.js
+++ source/js/modules/hero-slider.js
@@ -1,21 +1,28 @@
 import Swiper from 'swiper';
 import { Pagination } from 'swiper/modules';
 
+const swiperOptions = {
+  modules: [Pagination],
+  direction: 'horizontal',
+  slidesPerView: 1,
+  init: false,
+  slideActiveClass: 'hero__item--active',
+  slideClass: 'hero__item',
+  wrapperClass: 'hero__list',
+  loop: true,
+  pagination: {
+    el: '.hero__pagination',
+    clickable: true,
+  },
+  breakpoints: {
+    1440: {
+      allowTouchMove: false,
+    },
+  },
+};
 
 const initHeroSlider = () => {
-  const swiper = new Swiper('.hero__container', {
-    modules: [Pagination],
-    direction: 'horizontal',
-    slidesPerView: 1,
-    init: false,
-    slideClass: 'hero__item',
-    wrapperClass: 'hero__list',
-    loop: true,
-    pagination: {
-      el: '.hero__pagination',
-      clickable: true,
-    },
-  });
+  const swiper = new Swiper('.hero__container', swiperOptions);
 
   const container = document.querySelector('.hero__container');
 
