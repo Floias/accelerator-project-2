45a09c85e85809c63177b6bf44efc13c063a89a3
Floias <irina.butcher@gmail.com>
2024-08-14 17:38:12 +0500

    заканчивает состояния интерактивных элементов готовых блоков

--- source/js/main.js
+++ source/js/main.js
@@ -1,7 +1,3 @@
-// https://swiperjs.com/get-started#installation
-// import Swiper from "swiper";
-// import {Navigation, Pagination} from "swiper/modules";
-// import 'swiper/css';
 import { changesMenuState } from './modules/menu';
 import { initHeroSlider } from './modules/hero-slider';
 import { initToursSlider } from './modules/tours-slider';
