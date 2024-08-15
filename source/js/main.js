7cb0adc338a80ff43f3d14b9ce92f3c071b21b3f
Floias <irina.butcher@gmail.com>
2024-08-15 22:19:42 +0500

    начинает стилизацию блока обучение

--- source/js/main.js
+++ source/js/main.js
@@ -1,7 +1,8 @@
 import { changesMenuState } from './modules/menu';
 import { initHeroSlider } from './modules/hero-slider';
 import { initToursSlider } from './modules/tours-slider';
-
+import { initTrainingSlider } from './modules/training-slider';
 changesMenuState();
 initHeroSlider();
 initToursSlider();
+initTrainingSlider();
---
+++ source/js/modules/training-slider.js
@@ -0,0 +1,60 @@
+import Swiper from 'swiper';
+import { Navigation } from 'swiper/modules';
+
+const container = document.querySelector('.training__slider');
+const buttonPrev = container.querySelector('.training__button--prev');
+const buttonNext = container.querySelector('.training__button--next');
+
+const initTrainingSlider = () => {
+  const swiper = new Swiper('.training__slider', {
+    modules: [Navigation],
+    a11y: {
+      prevSlideMessage: 'Previous slide',
+      nextSlideMessage: 'Next slide',
+    },
+    direction: 'horizontal',
+    init: false,
+    slideActiveClass: 'training__item--active',
+    slideClass: 'training__item',
+    wrapperClass: 'training__list',
+    navigation: {
+      nextEl: '.training__button--next',
+      prevEl: '.training__button--prev',
+    },
+    breakpoints: {
+      320: {
+        slidesPerView: 1,
+      },
+      768: {
+        slidesPerView: 3,
+        spaceBetween: 18,
+      },
+      1440: {
+        slidesPerView: 4,
+        spaceBetween: 30,
+        allowTouchMove: false,
+      }
+    },
+    on: {
+      slideChange: () => {
+        if (swiper.isBeginning) {
+          buttonPrev.disabled = true;
+        } else {
+          buttonPrev.disabled = false;
+        }
+
+        if (swiper.isEnd) {
+          buttonNext.disabled = true;
+        } else {
+          buttonNext.disabled = false;
+        }
+      },
+    },
+  });
+
+  if (container) {
+    swiper.init(container);
+  }
+};
+
+export { initTrainingSlider };
