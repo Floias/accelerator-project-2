9cdd56e49206658cea2c9358adbe9d02ee0ec5bb
Floias <irina.butcher@gmail.com>
2024-08-18 15:17:52 +0500

    завершает блок о нас, исправляет ошибки

--- source/js/main.js
+++ source/js/main.js
@@ -2,7 +2,10 @@ import { changesMenuState } from './modules/menu';
 import { initHeroSlider } from './modules/hero-slider';
 import { initToursSlider } from './modules/tours-slider';
 import { initTrainingSlider } from './modules/training-slider';
+import { addsShadow } from './modules/about';
+
 changesMenuState();
 initHeroSlider();
 initToursSlider();
 initTrainingSlider();
+addsShadow();
---
+++ source/js/modules/about.js
@@ -0,0 +1,7 @@
+//data-title="Обучение"
+const title = document.querySelector('.about__content h2');
+const shadowValue = title.textContent;
+
+const addsShadow = () => title?.setAttribute('data-title', shadowValue);
+
+export { addsShadow };
